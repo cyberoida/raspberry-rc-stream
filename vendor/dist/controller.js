68ef074f792228475394f5f419cf8799937a1ccb
Patrick Pongratz <57094912+cyberoida@users.noreply.github.com>
2020-03-08 13:12:05 +0100

    add controller from old project

---
+++ vendor/dist/controller.js
@@ -0,0 +1,42 @@
+class Controller {
+    constructor(url) {
+      this.url = url;
+  
+      console.log("Trying to Connect");
+      // Websocket initialization
+      if (this.ws != undefined) {
+        this.ws.close();
+        delete this.ws;
+      }
+      this.ws = new WebSocket(url);
+      this.ws.binaryType = "arraybuffer";
+  
+      this.ws.onopen = () => {
+        console.log("Connected to " + url);
+      };
+    }
+  
+    Steer(amount) {
+      var message = "S" + amount;
+      this.ws.send(message);
+    }
+  
+    Gas(amount) {
+      var message = "A"+amount;
+      this.ws.send(message);
+    }
+  
+    Reverse(amount) {
+      var message = "R"+amount;
+      this.ws.send(message);
+    }
+  
+    Brake() {
+      var message = "B0";
+      this.ws.send(message);
+    }
+    Emergency() {
+      var message = "E0";
+      this.ws.send(message);
+    }
+  }
