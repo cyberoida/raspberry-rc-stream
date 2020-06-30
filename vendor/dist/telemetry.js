aba25e9cb1e9d4e27d091cfdbef81851d9d2ff63
Patrick Pongratz <57094912+cyberoida@users.noreply.github.com>
2020-06-30 15:54:39 +0200

    telemetry implementation

--- vendor/dist/telemetry.js
+++ vendor/dist/telemetry.js
@@ -1,23 +1,38 @@
 class Telemetry {
-    constructor(url) {
-      this.url = url;
-  
-      console.log("Trying to Connect");
-      // Websocket initialization
-      if (this.ws != undefined) {
-        this.ws.close();
-        delete this.ws;
-      }
-      this.ws = new WebSocket(url);
-      this.ws.binaryType = "arraybuffer";
-      
-  
-      this.ws.onopen = () => {
-        console.log("Connected to " + url);
-      };
+  constructor(url) {
+    this.url = url;
 
-      this.ws.addEventListener('message', function (event) {
-        //console.log('Message from server ', event.data);
-      });
+    console.log("Trying to Connect");
+    // Websocket initialization
+    if (this.ws != undefined) {
+      this.ws.close();
+      delete this.ws;
     }
+    this.ws = new WebSocket(url);
+    this.ws.binaryType = "arraybuffer";
+
+
+    this.ws.onopen = () => {
+      console.log("Connected to " + url);
+    };
+
+    this.ws.addEventListener('message', function (event) {
+      //console.log('Message from server ', event.data);
+
+      if (event.data.startsWith("S")) {
+        var signal = parseInt(event.data.substring(1))
+        document.getElementById('signal').innerHTML = signal + "%";
+      } else if (event.data.startsWith("C")) {
+        var carrier = event.data.substring(1)
+        document.getElementById('networkName').innerHTML = carrier;
+        document.getElementById('conn').innerHTML = "OK";
+      }
+      if (event.data.startsWith("N")) {
+        var gpslat = event.data.split(',')[0];
+        var gpslon = event.data.split(',')[1];
+        document.getElementById('gpslat').innerHTML = gpslat.substring(1);
+        document.getElementById('gpslon').innerHTML = gpslon.substring(1);
+      }
+
   }
+}
